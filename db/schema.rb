71027abb124f11a0a06d80cfd9341ee5b3b785a4
josh <josh@mojotech.com>
2015-01-16 19:49:36 -0500

    Create post model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150115160720) do
+ActiveRecord::Schema.define(version: 20150116161136) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,15 @@ ActiveRecord::Schema.define(version: 20150115160720) do
     t.string "email"
     t.string "password_hash"
     t.string "password_salt"
+  end
+
+  create_table "posts", force: true do |t|
+    t.string   "name"
+    t.integer  "user_id"
+    t.string   "description"
+    t.string   "items"
+    t.string   "tags"
+    t.datetime "created_at"
   end
 
   create_table "users", force: true do |t|
